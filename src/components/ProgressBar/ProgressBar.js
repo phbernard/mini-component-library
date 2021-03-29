bde5c217f46e7b61e2e7bead4636503906eb6e02
Philippe Bernard <philippe.bernard@gmail.com>
2021-03-29 10:35:43 +0200

    Progress

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,29 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Wrapper>
+    <ProgressContainer>
+      <Progress value={value}/>
+    </ProgressContainer>
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  padding: 5px;
+  background-color: lightgrey;
+  border-radius: 4px;
+  box-shadow: inset 0px 4px 4px grey;
+`;
+
+const ProgressContainer = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Progress = styled.div`
+  width: ${p => p.value}%;
+  height: 16px;
+  background-color: blue;
+`;
 
 export default ProgressBar;
